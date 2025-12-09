869042b1ac7d8a13717a62548ca2d084cf2919db
Adam Fox <adam@foxy.id.au>
2025-12-09 22:08:03 +1030

    day 5 part 2

--- internal/day05/day05.go
+++ internal/day05/day05.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+func inRange(x int, r [2]int) bool {
+	return x >= r[0] && x <= r[1]
+}
+
 func Part1(input []string) (string, error) {
 	var ids []int
 	var ranges [][2]int
@@ -64,5 +68,42 @@ func Part1(input []string) (string, error) {
 }
 
 func Part2(input []string) (string, error) {
-	return "unimplemented", nil
+	var ranges [][2]int
+	for _, line := range input {
+		if line == "" {
+			break
+		}
+		rangeInts := [2]int{}
+		for idx, x := range strings.Split(line, "-") {
+			y, _ := strconv.Atoi(x)
+			rangeInts[idx] = y
+		}
+		ranges = append(ranges, rangeInts)
+	}
+
+	sort.Slice(ranges, func(i, j int) bool {
+		if ranges[i][0] == ranges[j][0] {
+			return ranges[i][1] < ranges[j][1]
+		} else {
+			return ranges[i][0] < ranges[j][0]
+		}
+	})
+
+	var consolidatedRanges [][2]int = [][2]int{ranges[0]}
+	for _, origRange := range ranges {
+		lastIdx := len(consolidatedRanges) - 1
+		if inRange(origRange[0], consolidatedRanges[lastIdx]) {
+			if !inRange(origRange[1], consolidatedRanges[lastIdx]) {
+				consolidatedRanges[lastIdx][1] = origRange[1]
+			}
+		} else {
+			consolidatedRanges = append(consolidatedRanges, origRange)
+		}
+	}
+
+	total := 0
+	for _, r := range consolidatedRanges {
+		total += r[1] - r[0] + 1
+	}
+	return fmt.Sprintf("%d", total), nil
 }
--- internal/day05/day05_test.go
+++ internal/day05/day05_test.go
@@ -26,3 +26,23 @@ func TestPart1(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(want, got)
 }
+
+func TestPart2(t *testing.T) {
+	assert := assert.New(t)
+	input := `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`
+
+	want := "14"
+	got, err := Part2(strings.Split(input, "\n"))
+	assert.NoError(err)
+	assert.Equal(want, got)
+}
