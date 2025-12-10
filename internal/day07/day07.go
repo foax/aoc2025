ad17e4a45d762d07680b822a4ebdc2f910690f18
Adam Fox <adam@foxy.id.au>
2025-12-10 18:19:59 +1030

    day 7 part 2

--- internal/day07/day07.go
+++ internal/day07/day07.go
@@ -1,6 +1,27 @@
 package day07
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func countBeamOptions(grid []string, cache map[[2]int]int, startCoord [2]int) int {
+	if startCoord[0] < 0 || startCoord[1] < 0 || startCoord[0] >= len(grid) || startCoord[1] >= len(grid[0]) {
+		return 0
+	}
+	for row := startCoord[0]; row < len(grid)-1; row++ {
+		if grid[row+1][startCoord[1]] == '^' {
+			count, ok := cache[[2]int{row + 1, startCoord[1]}]
+			if !ok {
+				count = countBeamOptions(grid, cache, [2]int{row + 1, startCoord[1] - 1})
+				count += countBeamOptions(grid, cache, [2]int{row + 1, startCoord[1] + 1})
+				cache[[2]int{row + 1, startCoord[1]}] = count
+			}
+			return count
+		}
+	}
+	return 1
+}
 
 func Part1(input []string) (string, error) {
 	manifold := make([][]rune, len(input))
@@ -35,12 +56,11 @@ func Part1(input []string) (string, error) {
 		}
 	}
 
-	for _, row := range manifold {
-		fmt.Println(string(row))
-	}
 	return fmt.Sprintf("%d", splitCount), nil
 }
 
 func Part2(input []string) (string, error) {
-	return "unimplemented", nil
+	cache := make(map[[2]int]int)
+	count := countBeamOptions(input, cache, [2]int{0, strings.Index(input[0], "S")})
+	return fmt.Sprintf("%d", count), nil
 }
