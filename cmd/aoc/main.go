adbf6c43ed3ec1530cb85c9efccf2c01caab0ecc
Adam Fox <adam@foxy.id.au>
2025-12-15 09:28:53 +1030

    Improve use of slog logger

--- cmd/aoc/main.go
+++ cmd/aoc/main.go
@@ -26,10 +26,9 @@ const (
 	aoc_edition     = "aoc2025"
 )
 
-var days = map[int]struct {
-	Part1 func([]string) (string, error)
-	Part2 func([]string) (string, error)
-}{
+type PartSolver func([]string) (string, error)
+
+var days = map[int][]PartSolver{
 	1: {day01.Part1, day01.Part2},
 	2: {day02.Part1, day02.Part2},
 	3: {day03.Part1, day03.Part2},
@@ -58,49 +57,48 @@ func main() {
 		panic(fmt.Sprintf("Invalid log level provided: %v", *logLevelFlag))
 	}
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: logLevel,
-	}))
-	slog.SetDefault(logger)
+	})).With("aoc", aoc_edition, "section", "main")
 
-	slog.Info("Start", "aoc", aoc_edition, "section", "main", "loglevel", logLevel)
-	defer slog.Info("End")
-	slog.Debug("flag args", "aoc", aoc_edition, "section", "main", "args", flag.Args())
+	logger.Info("Start", "loglevel", logLevel)
+	defer logger.Info("End")
+	logger.Debug("flag args", "args", flag.Args())
 
 	args := flag.Args()
 	if len(args) == 0 {
-		slog.Error("No day argument passed on the command line", "aoc", aoc_edition, "section", "main")
+		logger.Error("No day argument passed on the command line")
 		os.Exit(1)
 	}
 	dayNum, err := strconv.Atoi(args[0])
 	if err != nil {
-		slog.Error("Invalid day argument passed on the command line", "aoc", aoc_edition, "section", "main", "arg", args[0], "error", err)
+		logger.Error("Invalid day argument passed on the command line", "arg", args[0], "error", err)
 		os.Exit(1)
 	}
 	day, ok := days[dayNum]
 	if !ok {
-		slog.Error("No solution implemented for requested day", "aoc", aoc_edition, "section", "main", "day", dayNum)
+		logger.Error("No solution implemented for requested day", "day", dayNum)
 		os.Exit(1)
 	}
 
 	var reader io.Reader
 	info, err := os.Stdin.Stat()
 	if err != nil {
-		slog.Error("Error checking stdin", "aoc", aoc_edition, "section", "main", "error", err)
+		logger.Error("Error checking stdin", "error", err)
 		os.Exit(1)
 	}
 	if (info.Mode() & os.ModeCharDevice) == 0 {
-		slog.Info("Reading input from stdin", "aoc", aoc_edition, "section", "main")
+		logger.Info("Reading input from stdin")
 		reader = os.Stdin
 	} else {
-		slog.Debug("stdin is a terminal (no piped input)", "aoc", aoc_edition, "section", "main")
+		logger.Debug("stdin is a terminal (no piped input)")
 		filename := fmt.Sprintf("%s/day%02d.txt", input_directory, dayNum)
 		reader, err = os.Open(filename)
 		if err != nil {
-			slog.Error("Error opening input file", "aoc", aoc_edition, "section", "main", "filename", filename, "error", err)
+			logger.Error("Error opening input file", "filename", filename, "error", err)
 			os.Exit(1)
 		}
-		slog.Info("Reading input from input file", "aoc", aoc_edition, "section", "main", "filename", filename)
+		logger.Info("Reading input from input file", "filename", filename)
 	}
 	scanner := bufio.NewScanner(reader)
 
@@ -109,21 +107,17 @@ func main() {
 	for scanner.Scan() {
 		input = append(input, scanner.Text())
 	}
-	slog.Debug("Lines of input read", "aoc", aoc_edition, "section", "main", "lines", len(input))
+	logger.Debug("Lines of input read", "lines", len(input))
 
-	slog.Info("Solving", "aoc", aoc_edition, "section", "main", "day", dayNum, "part", 1)
-	part1Result, err := day.Part1(input)
-	if err != nil {
-		slog.Error("Error solving", "aoc", aoc_edition, "section", "main", "day", dayNum, "part", 1, "error", err)
-		os.Exit(1)
+	for partIdx, part := range day {
+		partNum := partIdx + 1
+		logger.Info("Solving", "day", dayNum, "part", partNum)
+		result, err := part(input)
+		if err != nil {
+			logger.Error("Error solving", "day", dayNum, "part", partNum, "error", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s/day%d/part%d: %s\n", aoc_edition, dayNum, partNum, result)
+		logger.Info("Result", "part", partNum, "result", result)
 	}
-	slog.Info("Result", "aoc", aoc_edition, "section", "main", "part", 1, "result", part1Result)
-
-	slog.Info("Solving", "aoc", aoc_edition, "section", "main", "day", dayNum, "part", 2)
-	part2Result, err := day.Part2(input)
-	if err != nil {
-		slog.Error("Error solving Part 2", "aoc", aoc_edition, "section", "main", "day", dayNum, "error", err)
-		os.Exit(1)
-	}
-	slog.Info("Result", "aoc", aoc_edition, "section", "main", "part", 2, "result", part2Result)
 }
